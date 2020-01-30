da90c0aca6a5732ef425a185045cd0c6d6cd25db
singlapranshika <44138859+singlapranshika@users.noreply.github.com>
2020-01-30 13:58:52 +0530

    Add files via upload

---
+++ ICustomList.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Custom
+{
+    interface ICustomList
+    {
+       
+        void AddFirst(int data);
+        void AddLast(int data);
+        void Insert(int index, int data);
+
+
+        int IndexOf(int item);
+        bool Contains(int item);
+
+
+        void display();
+        int Get(int index);
+
+
+        void Sort();
+
+
+        bool Remove(int data);
+        bool RemoveAt(int position);
+    }
+}
